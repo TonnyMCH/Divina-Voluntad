84334aff81048484104dfa57487d049d264b9be5
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2024-12-17 13:33:13 -0600

    act

---
+++ contenido.js
@@ -0,0 +1,4 @@
+const contenido = {
+    Amada: "Amada Divinidad, Padre, Hijo y Espíritu Santo. Entro en tu querer divino, tomo y hago mío tu poder, la inmensidad de tu amor, tus actos, obras, penas y cualidades divinas en tu Santísima Humanidad para girar a través de este Santo Rosario, con ello sello, bendigo y consagro a todas las creaturas de todas las generaciones, Santísima Trinidad en tu divino amor te abrazo y empiezo",
+    descripcion: "Este es un ejemplo de contenido externo desde un archivo JavaScript."
+};
