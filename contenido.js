b9669e0eebbc58809e5befe2974129a99666186c
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2024-12-17 13:50:08 -0600

    act

--- contenido.js
+++ contenido.js
@@ -1,4 +1,4 @@
 const contenido = {
     Amada: "Amada Divinidad, Padre, Hijo y Espíritu Santo. Entro en tu querer divino, tomo y hago mío tu poder, la inmensidad de tu amor, tus actos, obras, penas y cualidades divinas en tu Santísima Humanidad para girar a través de este Santo Rosario, con ello sello, bendigo y consagro a todas las creaturas de todas las generaciones, Santísima Trinidad en tu divino amor te abrazo y empiezo",
-    descripcion: "Este es un ejemplo de contenido externo desde un archivo JavaScript."
+    Espiritu: "Me fundo en el orden de tu Gracia, en todo lo que has hecho y harás en nosotros los santificantes. <br />¡Ven Divino Espíritu!, haz desahogo de Tu obra en el Fiat Voluntas Tua. Danos pronto a conocer vuestra Voluntad, para que conociéndola, amemos y acojamos Tu primer acto completo, el cual es, La Divina Voluntad."
 };
