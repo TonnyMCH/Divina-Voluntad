f4766095c2c66953e5b8c6c6b6026a562f11de32
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2024-12-18 23:06:00 -0600

    Super actualizado

---
+++ contdolorosos.js
@@ -0,0 +1,14 @@
+const contdolorosos = {
+    amada3: "Amada Divinidad, Padre, Hijo y Espíritu Santo. Entro en tu querer divino, tomo y hago mío tu poder, la inmensidad de tu amor, tus actos, obras, penas y cualidades divinas en tu Santísima Humanidad para girar a través de este Santo Rosario, con ello sello, bendigo y consagro a todas las creaturas de todas las generaciones, Santísima Trinidad en tu divino amor te abrazo y empiezo.",
+
+    santo3: "Me fundo en el orden de tu Gracia, en todo lo que has hecho y harás en nosotros los santificantes.\n 									   ¡Ven Divino Espíritu!, haz desahogo de Tu obra en el Fiat Voluntas Tua. Danos pronto a conocer vuestra Voluntad, para que conociéndola, amemos y acojamos Tu primer acto completo, el cual es, La Divina Voluntad.",
+
+    giro3: "Jesús, me fundo en ti y en tu Divina Voluntad y entro en Tu acto único y eterno donde está en acto presente tu vida y todo.\n Santísima Trinidad, te doy mi te amo, te alabo, te bendigo, te glorifico, te agradezco por la obra de la Creación, Redención y Santificación; y en compañía de Nuestra Madre y Reina del Cielo, de Luisa, de la Iglesia Militante, Purgante y Triunfante recorro tu dolorosa Pasión en estos Misterios.\n Ofrezco este Rosario para que ‘Venga tu Reino’, que tu ‘Voluntad se haga como en el Cielo así en la tierra’ de nuestra alma.\n Santísima Trinidad te abrazo y empiezo.",
+
+    acto3: "Dios mío, perdóname; yo tuve la osadía de ofenderte y de rebelarme contra ti en el mismo instante en que Tú me Amabas.\n Me arrepiento de todo corazón de haberte ofendido.\n     Te ruego, te suplico que me concedas tu amargura, a fin de poder dolerme con ese mismo dolor con el que tú te doliste por mis pecados; dolor tan grande e intenso que te hizo sudar sangre.\n Madre Celestial, alcánzame de tu Jesús el suspirado perdón.\n 																     Yo propongo y prometo del modo más enérgico y absoluto nunca más volver a pecar. Amén.",
+
+    credo3: "Creo en Dios, Padre Todopoderoso, Creador del cielo y de la tierra.\n 													   Creo en Jesucristo su único Hijo Nuestro Señor, que fue concebido por obra y gracia del Espíritu Santo.\n 										  Nació de Santa María Virgen, padeció bajo el poder de Poncio Pilato, fue crucificado, muerto y sepultado, descendió a los infiernos, al tercer día resucitó de entre los muertos, subió a los cielos y está sentado a la derecha de Dios Padre, todopoderoso. Desde allí va a venir a juzgar a vivos y muertos.\n 									   Creo en el Espíritu Santo, la Santa Iglesia católica, la comunión de los santos, el perdón de los pecados, la resurrección de la carne y la vida eterna. Amén",
+
+    miste3: "1) La Agonía de Jesús en el Huerto de los Olivos.\n 2) Jesús es Flagelado.\n 3) Jesús Coronado de Espinas.\n 4) Jesús con la Cruz a Cuestas.\n 5) Crucifixión y Muerte de Jesús."
+
+};
---
+++ contgozosos.js
@@ -0,0 +1,14 @@
+const contgozosos = {
+    amada4: "Amada Divinidad, Padre, Hijo y Espíritu Santo. Entro en tu querer divino, tomo y hago mío tu poder, la inmensidad de tu amor, tus actos, obras, penas y cualidades divinas en tu Santísima Humanidad para girar a través de este Santo Rosario, con ello sello, bendigo y consagro a todas las creaturas de todas las generaciones, Santísima Trinidad en tu divino amor te abrazo y empiezo.",
+
+    santo4: "Me fundo en el orden de tu Gracia, en todo lo que has hecho y harás en nosotros los santificantes.\n ¡Ven Divino Espíritu!, haz desahogo de Tu obra en el Fiat Voluntas Tua. Danos pronto a conocer vuestra Voluntad, para que conociéndola, amemos y acojamos Tu primer acto completo, el cual es, La Divina Voluntad.",
+
+    giro4: "Fundida en ti Jesús y en tu Divina Voluntad, en unión, a mi Madre María, a Luisa, a la Iglesia militante, triunfante y purgante recorro en tu acto único y eterno los misterios que inundan el Gozo de tu vida, dando en cada acto y pena Tuya mi te Amo con tu Voluntad, te Alabo, te Bendigo, te Glorifico y te Abrazo a nombre de todos y por todos.\n Ofrezco este Santo Rosario para que ‘Venga tu Reino’, que tu ‘Voluntad se haga como en el Cielo así en la tierra’ de nuestra alma, por la conversión, vida y Salvación de las almas.\n Y pronunciando mi Fiat en Tu FIAT Divino, abrazo mi alma a la Tuya y empiezo.",
+
+    acto4: "Dios mío, perdóname; yo tuve la osadía de ofenderte y de rebelarme contra ti en el mismo instante en que Tú me Amabas.\n Me arrepiento de todo corazón de haberte ofendido.\n Te ruego, te suplico que me concedas tu amargura, a fin de poder dolerme con ese mismo dolor con el que tú te doliste por mis pecados; dolor tan grande e intenso que te hizo sudar sangre.\n Madre Celestial, alcánzame de tu Jesús el suspirado perdón.\n Yo propongo y prometo del modo más enérgico y absoluto nunca más volver a pecar. Amén.",
+
+    credo4: "Creo en Dios, Padre Todopoderoso, Creador del cielo y de la tierra.\n Creo en Jesucristo su único Hijo Nuestro Señor, que fue concebido por obra y gracia del Espíritu Santo.\n Nació de Santa María Virgen, padeció bajo el poder de Poncio Pilato, fue crucificado, muerto y sepultado, descendió a los infiernos, al tercer día resucitó de entre los muertos, subió a los cielos y está sentado a la derecha de Dios Padre, todopoderoso. Desde allí va a venir a juzgar a vivos y muertos.\n Creo en el Espíritu Santo, la Santa Iglesia católica, la comunión de los santos, el perdón de los pecados, la resurrección de la carne y la vida eterna. Amén",
+
+    miste4: "1) La Anunciación.\n 2) La Visitación.\n 3) El Nacimiento de Jesús.\n 4) La Presentación de Jesús al Templo.\n 5) Jesús perdido y hallado en el Templo."
+
+};
