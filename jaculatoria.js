dd9e5cbee7d2e97f8d53cd4fbe57efc5e87eb469
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2024-12-29 21:30:38 -0600

    act

--- jaculatoria.js
+++ jaculatoria.js
@@ -1,6 +1,4 @@
-const jacularia = { 
-
-una: "Oh mi buen Jesús, perdona nuestros pecados, líbranos del fuego del infierno, y lleva al cielo a todas las almas, especialmente a las más necesitadas  de Tu infinita misericordia.  Amén. n\n\“Jesús: yo creo en tu amor por mí; hago mío todo y a todos en tu Voluntad porque te amo, y quiero amarte,alabarte, darte gracias, repararte y compensarte, por mí y por todos en tu Divina Voluntad, junto con María Santísima y Luisa”". 
-
-
+const jaculatoria =
+{
+    holas:"Oh mi buen Jesús, perdona nuestros pecados, líbranos del fuego del infierno, y lleva al cielo a todas las almas, especialmente a las más necesitadas  de Tu infinita misericordia.  Amén \n\n “Jesús: yo creo en tu amor por mí; hago mío todo y a todos en tu Voluntad porque te amo, y quiero amarte, alabarte, darte gracias, repararte y compensarte, por mí y por todos en tu Divina Voluntad, junto con María Santísima y Luisa”. "
 };
